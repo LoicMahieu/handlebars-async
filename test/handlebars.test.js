17be3d146c186ab5950b36e2acca82430786f855
Loïc Mahieu <mahieuloic@gmail.com>
2013-09-19 01:41:17 +0200

    Fix partials

--- test/handlebars.test.js
+++ test/handlebars.test.js
@@ -64,4 +64,22 @@ describe('Handlebars Async', function () {
     });
   });
 
+  it('Can register a async helper with a partial', function (done) {
+    hbs.registerHelper('asyncHelper', function (arg1, options) {
+      var callback = this.async();
+      process.nextTick(function () {
+        return callback(null, arg1.toUpperCase() + '-' + options.hash.arg2);
+      });
+    });
+
+    hbs.registerPartial('partials/thePartial', 'Hello Iam a partial');
+
+    var tpl = hbs.compile('{{asyncHelper "value" arg2="value2"}}{{> "partials/thePartial"}}');
+
+    tpl(function (err, content) {
+      expect(content).to.be.equal("VALUE-value2Hello Iam a partial");
+      done();
+    });
+  });
+
 });
